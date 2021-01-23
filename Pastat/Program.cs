03b87048994337bdcffa50e40c82c446e117b32f
Творческий Человек <accuratealx@gmail.com>
2021-01-23 20:09:15 +0300

    Fix some errors

--- Pastat/Program.cs
+++ Pastat/Program.cs
@@ -14,7 +14,7 @@ namespace PascalCodeStats
             Console.BufferWidth = 110;
             Console.WindowHeight = 50;
 
-            Console.WriteLine("Pastat v1.0 (23.01.2021)  <Cherry Programmer. medulla_261@mail.ru>");
+            Console.WriteLine("Pastat v1.0 (23.01.2021)  <Cheery Programmer. medulla_261@mail.ru>");
             Console.WriteLine("Special for Simple Game Engine");
 
             string currentDir = Directory.GetCurrentDirectory();
