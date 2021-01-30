1e304949a275d3d386eadfd483dc3ac3a00ab867
Sergey V <medulla_261@mail.ru>
2021-01-30 15:53:54 +0300

    Fixes search by extensions. Small refactorings.

--- Pastat/Program.cs
+++ Pastat/Program.cs
@@ -2,103 +2,91 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 
 namespace PascalCodeStats
 {
 	class Program
 	{
+		private static readonly HashSet<string> FilesToSearch = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+		{
+			".pas", ".inc", ".pp", ".lpr", ".dpr"
+		};
+
 		static void Main(string[] args)
 		{
-            Console.WindowWidth = 110;
-            Console.BufferWidth = 110;
-            Console.WindowHeight = 50;
+			Console.WindowWidth = 110;
+			Console.BufferWidth = 110;
+			Console.WindowHeight = 50;
 
-            Console.WriteLine("Pastat v1.1 23.01.2021  [Cheery Programmer   medulla_261@mail.ru]");
-            Console.WriteLine("Special for Simple Game Engine");
+			Console.WriteLine("Pastat v1.1 23.01.2021  [Cheery Programmer   medulla_261@mail.ru]");
+			Console.WriteLine("Special for Simple Game Engine");
 
-            string currentDir = Directory.GetCurrentDirectory();
+			string currentDir = Directory.GetCurrentDirectory();
 			string[] filePaths = EnumerateFilesRecursively(currentDir).ToArray();
 
-            if (filePaths.Length > 0)
-            {
-                string tableline =      $"------------+---------+--------------+-----------+--------+--------------------------------------------------";
-                string tablehead =      $"Строки      | Код     | Комментарии  | Директивы | Пустые | Имя файла";
-                string tableheadtotal = $"Cтрок       | Кода    | Комментариев | Директив  | Пустых | Файлов";
+			if (filePaths.Length > 0)
+			{
+				string tableline = $"------------+---------+--------------+-----------+--------+--------------------------------------------------";
+				string tablehead = $"Строки      | Код     | Комментарии  | Директивы | Пустые | Имя файла";
+				string tableheadtotal = $"Cтрок       | Кода    | Комментариев | Директив  | Пустых | Файлов";
 
-                //Таблица
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine(tableline);
-                Console.WriteLine(tablehead);
-                Console.WriteLine(tableline);
-                FileScanner scanner = new FileScanner();
-                TotalStatsCollector statsCollector = new TotalStatsCollector();
-                foreach (string filePath in filePaths)
-                {
-                    var stats = scanner.Scan(filePath);
-                    statsCollector.Add(stats);
-                    Console.WriteLine(ReadStatsLine(stats));
-                }
-                Console.WriteLine(tableline);
+				//Таблица
+				Console.WriteLine();
+				Console.WriteLine();
+				Console.WriteLine(tableline);
+				Console.WriteLine(tablehead);
+				Console.WriteLine(tableline);
+				FileScanner scanner = new FileScanner();
+				TotalStatsCollector statsCollector = new TotalStatsCollector();
+				foreach (string filePath in filePaths)
+				{
+					var stats = scanner.Scan(filePath);
+					statsCollector.Add(stats);
+					Console.WriteLine(ReadStatsLine(stats));
+				}
+				Console.WriteLine(tableline);
 
-                //Итого
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("                                                    Всего:");
-                Console.WriteLine(tableline);
-                Console.WriteLine(tableheadtotal);
-                Console.WriteLine(tableline);
-                Console.WriteLine($"{statsCollector.TotalLines, -12}" +
-                                  $"| {statsCollector.CodeCount, -8}" +
-                                  $"| {statsCollector.CommentsCount, -13}" +
-                                  $"| {statsCollector.DirectivesCount, -10}" +
-                                  $"| {statsCollector.EmptyLinesCount, -7}" +
-                                  $"| {filePaths.Length}");
-                Console.WriteLine(tableline);
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("файлов не найдено");
-            }
+				//Итого
+				Console.WriteLine();
+				Console.WriteLine();
+				Console.WriteLine("                                                    Всего:");
+				Console.WriteLine(tableline);
+				Console.WriteLine(tableheadtotal);
+				Console.WriteLine(tableline);
+				Console.WriteLine($"{statsCollector.TotalLines,-12}" +
+								  $"| {statsCollector.CodeCount,-8}" +
+								  $"| {statsCollector.CommentsCount,-13}" +
+								  $"| {statsCollector.DirectivesCount,-10}" +
+								  $"| {statsCollector.EmptyLinesCount,-7}" +
+								  $"| {filePaths.Length}");
+				Console.WriteLine(tableline);
+			}
+			else
+			{
+				Console.WriteLine();
+				Console.WriteLine("файлов не найдено");
+			}
 
-            Console.ReadKey();
-        }
+			Console.ReadKey();
+		}
 
 		private static string ReadStatsLine(FileStats stats)
 		{
-			return $"{stats.TotalLines, -12}" +
-				$"| {stats.CodeCount, -8}" +
-				$"| {stats.CommentsCount, -13}" +
-				$"| {stats.DirectivesCount, -10}" +
-                $"| {stats.EmptyLinesCount, -7}" +
-                $"| {Path.GetFileName(stats.FullPath)}";
+			return $"{stats.TotalLines,-12}" +
+				$"| {stats.CodeCount,-8}" +
+				$"| {stats.CommentsCount,-13}" +
+				$"| {stats.DirectivesCount,-10}" +
+				$"| {stats.EmptyLinesCount,-7}" +
+				$"| {Path.GetFileName(stats.FullPath)}";
 		}
 
 		static IEnumerable<string> EnumerateFilesRecursively(string path)
 		{
-			foreach (string filePath in EnumerateFiles(path))
-                yield return filePath;
-
-
-
-			foreach(string dirPath in Directory.EnumerateDirectories(path))
-			{
-				foreach (string nestedFile in EnumerateFilesRecursively(dirPath))
-					yield return nestedFile;
-			}
+			return FilesToSearch
+				.Select(ext => $"*{ext}")
+				.SelectMany(pattern => Directory.EnumerateFiles(path, pattern, SearchOption.AllDirectories))
+				.Where(filePath => FilesToSearch.Contains(Path.GetExtension(filePath)));
 		}
-
-        static IEnumerable<string> EnumerateFiles(string path)
-        {
-            return Directory.EnumerateFiles(path, "*.pas")
-                .Concat(Directory.EnumerateFiles(path, "*.inc"))
-                .Concat(Directory.EnumerateFiles(path, "*.pp"))
-                .Concat(Directory.EnumerateFiles(path, "*.lpr"))
-                .Concat(Directory.EnumerateFiles(path, "*.dpr"));
-        }
-
-    }
+	}
 }
 
